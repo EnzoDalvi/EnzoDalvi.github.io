fa199a974cc5116dab2340280ad80704b54764a3
EnzoDalvi <43136538+EnzoDalvi@users.noreply.github.com>
2020-11-28 15:49:51 -0300

    Update loginScript.js

--- Projeto_Site/login_folder/loginScript.js
+++ Projeto_Site/login_folder/loginScript.js
@@ -9,7 +9,7 @@ function logar(){
         sub.style.visibility= "visible"
     }
    else //if(username == userc && senha == senhac){
-        window.alert("Você está logado")  
-        window.location.replace("https://enzodalvi.github.io/Projeto_Site/home_folder/pag_home.html");
+      //  window.alert("Você está logado")  
+      //  window.location.replace("https://enzodalvi.github.io/Projeto_Site/home_folder/pag_home.html");
    // }
 }   
